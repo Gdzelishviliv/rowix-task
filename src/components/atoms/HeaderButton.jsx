3147886377b1408acd76a7b6310710ef5b1673bb
Nanno <gdzelishviliv@gmail.com>
2025-12-02 17:23:16 +0400

    feat[fix]-header component

--- src/components/atoms/HeaderButton.jsx
+++ src/components/atoms/HeaderButton.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const HeaderButton = ({ name, variants }) => {
   const baseStyles =
-    "flex justify-center items-center h-[63px] px-6 py-[18px] rounded-xl cursor-pointer";
+    "flex justify-center uppercase items-center text-[18px] h-[63px] px-6 py-[18px] rounded-xl cursor-pointer font-medium font-mono";
   const styles =
     variants === "contact"
       ? "text-[#0F0F0F] bg-[#CE7D63]"
--- src/components/organisms/Header.jsx
+++ src/components/organisms/Header.jsx
@@ -3,6 +3,16 @@ import HeaderLogo from "../../assets/HeaderLogo.svg";
 import HeaderButton from "../atoms/HeaderButton";
 
 const Header = () => {
+  const headerItems = [
+    { name: "home" },
+    { name: "services" },
+    { name: "projects" },
+    { name: "about" },
+    { name: "contact" },
+    { name: "blogs" },
+    { name: "contact us", variant: "contact" },
+  ];
+
   return (
     <header className="flex p-6 pl-[50px] h-[111px] rounded-2xl justify-between items-center bg-[#1A1A1A]">
       <img
@@ -11,15 +21,15 @@ const Header = () => {
         width={129.7071075439453}
         height={24}
       />
+
       <div className="flex justify-between gap-4">
-        <HeaderButton name="HOME" />
-        <HeaderButton name="Services" />
-        <HeaderButton name="Projects" />
-        <HeaderButton name="About" />
-        <HeaderButton name="Contact" />
-        <HeaderButton name="Blogs" />
-        <HeaderButton name="Contact Us" variants="contact" />
-        
+        {headerItems.map((item) => (
+          <HeaderButton
+            key={item.name}
+            name={item.name}
+            variants={item.variant}
+          />
+        ))}
       </div>
     </header>
   );
